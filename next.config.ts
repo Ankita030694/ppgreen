5edc7cbee1b6746c3b38cb738d9f0eacf89de508
ZeD31 <mohdzaib318@gmail.com>
2026-07-27 11:53:07 +0530

    Images fixed + Contetn added

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
-const nextConfig: NextConfig = {};
-=======
 const nextConfig: NextConfig = {
   // @ts-expect-error: NextConfig type might be missing this field but it works
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
->>>>>>> 3446f2eaee5fcbc1426e157e7f4ba4bc9a484d65
 
 export default nextConfig;
